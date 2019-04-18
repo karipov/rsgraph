86cacc18af7f72c8c91ba227f99c9567c4edf675
Komron <komron_aripov@cdl.ch>
2019-04-18 14:25:01 +0200

    re-scope methods for use

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,17 @@
 #![allow(unused_imports)]
 
 mod types;
+mod methods;
 
 use types::{
     account,
     node, node_element,
-    page
+    page,
+};
+
+use methods::{
+    create_account,
+    create_page,
+    get_account_info,
+    revoke_access_token,
 };
---
+++ src/methods.rs
@@ -0,0 +1,5 @@
+pub mod create_account;
+pub mod create_page;
+pub mod edit_account_info;
+pub mod get_account_info;
+pub mod revoke_access_token;
